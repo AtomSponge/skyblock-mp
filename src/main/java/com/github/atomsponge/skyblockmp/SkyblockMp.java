a8765c1c5889eb683fd4d785cdf637f9c36c59ac
AtomSponge <mratomsponge@gmail.com>
2016-03-03 19:43:42 +0100

    Add configuration file

--- src/main/java/com/github/atomsponge/skyblockmp/SkyblockMp.java
+++ src/main/java/com/github/atomsponge/skyblockmp/SkyblockMp.java
@@ -1,12 +1,16 @@
 package com.github.atomsponge.skyblockmp;
 
+import com.github.atomsponge.skyblockmp.util.ConfigUtils;
+import com.typesafe.config.Config;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import lombok.Getter;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = Constants.MOD_ID, name = Constants.MOD_NAME, version = Constants.VERSION)
+import java.io.IOException;
+
+@Mod(modid = Constants.MOD_ID, name = Constants.MOD_NAME, version = Constants.VERSION, acceptableRemoteVersions = "*")
 public class SkyblockMp {
     @Getter
     @Mod.Instance
@@ -14,9 +18,18 @@ public class SkyblockMp {
 
     @Getter
     private Logger logger;
+    @Getter
+    private Config config;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         logger = event.getModLog();
+
+        try {
+            logger.info("Loading config");
+            config = ConfigUtils.load(getClass().getClassLoader(), "config.conf", event.getSuggestedConfigurationFile());
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load config", e);
+        }
     }
 }
---
+++ src/main/java/com/github/atomsponge/skyblockmp/util/ConfigUtils.java
@@ -0,0 +1,58 @@
+package com.github.atomsponge.skyblockmp.util;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.ConfigRenderOptions;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author AtomSponge
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ConfigUtils {
+    /**
+     * Loads a {@link com.typesafe.config.Config} from a specified {@link java.io.File}.
+     * Creates the file with the default values if it does not exist.
+     * If a value does not exist in the file, the value from the default config will be added to the file.
+     *
+     * @param loader   The class loader
+     * @param resource The path to the default config resource
+     * @param file     The file
+     * @return The loaded config
+     * @throws java.io.IOException
+     */
+    public static Config load(ClassLoader loader, String resource, File file) throws IOException {
+        Config defaults = ConfigFactory.parseResources(loader, resource);
+        Config config = ConfigFactory.parseFile(file).withFallback(defaults);
+
+        ConfigRenderOptions renderOptions = ConfigRenderOptions.defaults().setJson(false).setOriginComments(false);
+        try (PrintWriter printWriter = new PrintWriter(file)) {
+            String rendered = config.root().render(renderOptions);
+            String[] lines = rendered.split(System.lineSeparator());
+
+            // Only indent two spaces
+            for (int i = 0; i < lines.length; i++) {
+                lines[i] = lines[i].replace("    ", "  ");
+            }
+
+            // New line after values
+            for (int i = 0; i < lines.length - 1; i++) {
+                String line = lines[i];
+                if (!line.trim().startsWith("#") && // No new line after comments
+                        !line.endsWith("{") && // No new line after "block beginnings"
+                        !lines[i + 1].trim().equals("}")) { // No new line after "block endings"
+                    lines[i] += System.lineSeparator();
+                }
+            }
+
+            printWriter.write(StringUtils.join(lines, System.lineSeparator()));
+        }
+        return config;
+    }
+}
