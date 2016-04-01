6c06ce9b48a7d4e8dfc986bc1118506495a7bc4e
AtomSponge <mratomsponge@gmail.com>
2016-04-01 21:22:42 +0200

    Minor database SQL script changes

--- src/main/resources/database-updates/version-1.sql
+++ src/main/resources/database-updates/version-1.sql
@@ -1,14 +1,21 @@
-CREATE TABLE players (
-  id            INT         NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  uuid          BINARY(16)  NOT NULL UNIQUE,
-  last_username VARCHAR(16) NOT NULL UNIQUE
+CREATE TABLE player (
+  id             INT         NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  uuid           BINARY(16)  NOT NULL UNIQUE,
+  last_username  VARCHAR(16) NOT NULL UNIQUE,
+  default_island INT
 );
 
-CREATE TABLE islands (
-  id      INT       NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  owner   INT       NOT NULL,
-  created TIMESTAMP NOT NULL             DEFAULT CURRENT_TIMESTAMP(),
-  pos_x   INT       NOT NULL,
-  pos_z   INT       NOT NULL,
-  FOREIGN KEY (owner) REFERENCES players (id) ON DELETE CASCADE
+CREATE TABLE island (
+  id       INT       NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  owner    INT       NOT NULL,
+  created  TIMESTAMP NOT NULL             DEFAULT CURRENT_TIMESTAMP(),
+  pos_x    INT       NOT NULL,
+  pos_z    INT       NOT NULL,
+  offset_x DOUBLE    NOT NULL             DEFAULT 0.0,
+  offset_y DOUBLE    NOT NULL             DEFAULT 0.0,
+  offset_z DOUBLE    NOT NULL             DEFAULT 0.0,
+  FOREIGN KEY (owner) REFERENCES player (id) ON UPDATE CASCADE
 );
+
+// Can't create a foreign key referencing a table which doesn't exist yet...
+ALTER TABLE player ADD FOREIGN KEY (default_island) REFERENCES island (id);
